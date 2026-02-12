34da2c460f71f5f6316e7b919874f834d962cbef
leederic9306 <leederic9306@gmail.com>
2026-02-12 18:45:59 +0900

    [BOJ] 24480 - 알고리즘 수업 - 깊이 우선 탐색 2 (Java)

---
+++ src/baekjoon/dfs_bfs/_24480/Main.java
@@ -0,0 +1,59 @@
+package baekjoon.dfs_bfs._24480;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int seq = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < M; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st2.nextToken());
+            int y = Integer.parseInt(st2.nextToken());
+
+            graph.get(x).add(y);
+            graph.get(y).add(x);
+        }
+
+        for(int i = 0; i < N  + 1; i++) {
+            Collections.sort(graph.get(i), Collections.reverseOrder());
+        }
+
+        int[] visited = new int[N + 1];
+        dfs(graph, visited, R);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static void dfs(List<List<Integer>> graph, int[] visited, int pos) {
+        visited[pos] = seq;
+        seq++;
+        for(int i : graph.get(pos)) {
+            if(visited[i] == 0) {
+                dfs(graph, visited, i);
+            }
+        }
+    }
+}
+
