908ada1f3f6fdcf7fcccc2e725cbcd1a8173effc
leederic9306 <leederic9306@gmail.com>
2026-03-24 22:41:26 +0900

    [POG] level1 - 완주하지 못한 선수   (Java)

--- src/programmers/level1/Solution2.java
+++ src/programmers/level1/Solution2.java
@@ -9,21 +9,18 @@ class Solution2 {
         String answer = "";
         Map<String, Integer> person = new HashMap<>();
 
-        // 1. 참가자 등록: getOrDefault로 if문 없이 깔끔하게 카운팅
         for (String part : participant) {
             person.put(part, person.getOrDefault(part, 0) + 1);
         }
 
-        // 2. 완주자 차감
         for (String comp : completion) {
             person.put(comp, person.get(comp) - 1);
         }
 
-        // 3. 완주하지 못한 사람 찾기: entrySet()으로 탐색 비용 최소화
         for (Map.Entry<String, Integer> entry : person.entrySet()) {
             if (entry.getValue() != 0) {
                 answer = entry.getKey();
-                break; // 정답을 찾았으므로 즉시 반복문 종료 (효율성 UP)
+                break;
             }
         }
 
